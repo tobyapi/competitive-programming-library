ee778b1ff9e4721f304d769ef56776a74da2bd51
Ryuya Takahashi <ryuya@takahashi.holiday>
2022-01-29 00:20:09 +0900

    add ccw

--- go/geometry/utils.go
+++ go/geometry/utils.go
@@ -25,3 +25,18 @@ func Sign2(a float64, b float64) int {
 func Sqrt(a float64) float64 {
 	return math.Sqrt(math.Max(a, 0.0))
 }
+
+func CCW(a, b, c Point) int {
+	p := b.Sub(a)
+	q := c.Sub(a)
+	if Cross(p, q) > 0 {
+		return 1
+	} else if Cross(p, q) < 0 {
+		return -1
+	} else if Dot(p, q) < 0 {
+		return 2
+	} else if p.Norm() < c.Norm() {
+		return -2
+	}
+	return 0
+}
